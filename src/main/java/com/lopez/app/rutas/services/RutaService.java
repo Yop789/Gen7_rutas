fdc5591c7b7b4f4673525c1474e1b1f141b6dd51
Yop789 <2000djpz@gmail.com>
2024-06-29 00:37:09 -0600

    todo

--- src/main/java/com/lopez/app/rutas/services/RutaService.java
+++ src/main/java/com/lopez/app/rutas/services/RutaService.java
@@ -2,7 +2,7 @@ package com.lopez.app.rutas.services;
 
 import java.sql.Connection;
 import java.sql.SQLException;
-import java.util.ArrayList;
+
 import java.util.List;
 import java.util.Optional;
 
